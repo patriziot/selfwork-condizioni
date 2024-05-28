0f8974d401169faeb5eb86fab98f75358d260469
patriziot <52088452+patriziot@users.noreply.github.com>
2024-05-28 17:31:56 +0200

    Selfwork condizioni 2

---
+++ selfwork-condizioni-2/main.js
@@ -0,0 +1,37 @@
+
+//Esercizio con If Else
+let temperatura = -8
+if(temperatura <20 && temperatura > 0){console.log('non ci sono piu’ le mezze stagioni')}
+else if(temperatura >= 30){console.log('lu mare, lu sole, lu ientu')}
+else if(temperatura <30 && temperatura >=20){console.log('mi dia una Peroni sudata')}
+else if(temperatura <0 && temperatura >= -9){console.log('non è tanto il freddo quanto la umidità')}
+else if(temperatura <-10){console.log('copriti...ancora ti raffreddi')} 
+
+
+// Esercizio con Switch
+
+
+// let temperatura = 0;
+// let avviso;
+
+// switch(true){
+//     case temperatura <20 && temperatura >0:
+//         avviso ='non ci sono piu’ le mezze stagioni'
+//         break;
+//     case temperatura >= 30:
+//         avviso = 'lu mare, lu sole, lu ientu'
+//         break;
+//     case temperatura <30 && temperatura >= 20:
+//         avviso = 'mi dia una Peroni sudata'
+//     case temperatura <=0 && temperatura >=-9:
+//         avviso = "non è tanto il freddo quanto l' umidità"
+//         break;
+//     case temperatura <=-10:
+//         avviso = 'copriti...ancora ti raffreddi'
+// }
+
+// console.log(avviso);
+
+
+
+
